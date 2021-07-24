69a636193f15e923cfebb4ce653eb0c0892ebea8
humaunkabir <humaunkabi85@gmail.com>
2021-07-24 18:13:57 +0600

    forth commit after creating user schemamodel and auth rout

---
+++ new-folder/models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      min: 6,
+    },
+    any: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+      default: '',
+    },
+    coverPicture: {
+      type: String,
+      default: '',
+    },
+    follower: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    timestamps: true,
+  }
+  // { timestamps: true }
+)
+
+module.exports = new mongoose.model('user', UserSchema)
