6b78c1576d750a6631e854b564e532f3936bd605
humaunkabir <humaunkabi85@gmail.com>
2021-07-24 15:02:28 +0600

    third commit

--- new-folder/index.js
+++ new-folder/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
 require('dotenv').config()
+const morgan = require('morgan')
+const helmet = require('helmet')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATA_BASE, { useNewUrlParser: true }, (param) => {
+  console.log(param)
+  console.log('db connected')
+})
+
 const app = express()
 
-const POTR = process.env.PORT
+app.use(morgan())
+app.use(helmet())
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('hello from lama dev social media app')
+})
 
 app.listen(PORT, () => console.log(`server listenning on port ${PORT}`))
