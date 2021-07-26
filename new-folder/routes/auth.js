6d556358b5da00e6922e3ae5644ce1dcac106b4b
humaunkabir <humaunkabi85@gmail.com>
2021-07-26 18:43:08 +0600

    seventh commit in this commit created the login route

--- new-folder/routes/auth.js
+++ new-folder/routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
 
+// register route
 router.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body
@@ -30,6 +31,33 @@ router.post('/register', async (req, res) => {
     console.log({ user })
     res.status(201).json({ user })
     // res.json(user)
+  } catch (err) {
+    console.log({ err })
+    res.status(500).json({ err })
+  }
+})
+
+//login route
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+      return res.json({ message: 'you have to fill this properly' })
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(404).json({ message: 'user not found' })
+    }
+    const validpass = await bcrypt.compare(password, user.password)
+    if (!validpass) {
+      return res.status(400).json({ message: 'invalid credintial' })
+    }
+    // console.log('now we can gio to save')
+
+    res.status(200).json(user)
   } catch (err) {
     console.log({ err })
     res.status(500).json({ err })
